7035863039e86a7523fe661119b7bd2f6302eca0
Nikolay Rogovoy <nikolay.rogovoy@gmail.com>
2017-07-14 15:33:50 +0300

    aot работает

--- copy-dist-files.js
+++ copy-dist-files.js
@@ -6,6 +6,6 @@ var resources = [
 ];
 resources.map(function(f) {
     var path = f.split('/');
-    var t = 'aot/' + path[path.length-1];
+    var t = 'src/' + path[path.length-1];
     fs.createReadStream(f).pipe(fs.createWriteStream(t));
 });
--- rollup-config.js
+++ rollup-config.js
@@ -25,6 +25,6 @@ export default {
         'node_modules/rxjs/**'
       ]
     }),
-    //uglify()
+    uglify()
   ]
 };
